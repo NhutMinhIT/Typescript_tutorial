5e5caa04effe9c3d092886ccb0ade4bc70da2878
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-03-04 21:22:01 +0700

    lesson02 TS

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = "NhutMinh ne!!";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "NhutMinh";
+meaningOfLife = 23;
+isLoading = true;
+album = "Khong xac dinh type";
+// album =56
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ lesson02/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = "NhutMinh ne!!";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = "NhutMinh";
+meaningOfLife = 23;
+isLoading = true;
+album = "Khong xac dinh type";
+// album =56
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
